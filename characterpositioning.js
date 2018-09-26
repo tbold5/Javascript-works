b4ed6b2304525a8b7c049772b3b1da3850ef2e3f
Tuguldur <tuguldurbold5@gmail.com>
2018-09-26 20:25:08 +0000

    changed

--- characterpositioning.js
+++ characterpositioning.js
@@ -1,19 +1,3 @@
-// var myObject = {};
-
-//   function countLetters (str) {
-//     var noSpace = str.split(' ').join('');
-
-//     for (var i = 0; i < noSpace.length; i++) {
-//       if (myObject[noSpace[i]]) {
-//         myObject[noSpace[i]]++;
-//       } else {
-//         myObject[noSpace[i]] = 0;
-//         myObject[noSpace[i]] ++;
-//       }
-//     }
-//     return myObject;
-// }
-// console.log(countLetters('lighthouse in the house'));
 
 var myObject = {};
 
